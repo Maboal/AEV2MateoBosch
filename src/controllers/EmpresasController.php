0adead8150864e4d4333f070c6d1b37672628757
Maboal <maboal@floridauniversitaria.es>
2022-12-11 13:58:51 +0000

    punto 9.10 (insertar empresa) implementado

--- src/controllers/EmpresasController.php
+++ src/controllers/EmpresasController.php
@@ -24,4 +24,34 @@ class EmpresasController extends AbstractController
         ]);
     }
 
+    public function insert()
+    {   
+        if (!isset($_POST['nombre']) && !isset($_POST['cif']) && !isset($_POST['tipo'])){
+        $this->render("layout.html.twig", [
+            "action" => "/insert",
+            "form_name" => "Insertar Empresa",
+            "value_submit" => "Insertar"
+        ]);
+        };
+
+        if (isset($_POST['nombre']) && isset($_POST['cif']) && isset($_POST['tipo'])){
+            // Obtenemos el objeto EntityManager para poder realizar la insercion
+            $entityManager = (new EntityManager())->getEntityManager();
+            // Creamos nuevo objeto entidad
+            $newEmpresa = new EmpresasEntity();
+            // Settear valores
+            $newEmpresa->setNombre($_POST['nombre']);
+            $newEmpresa->setCif($_POST['cif']);
+            $newEmpresa->setTipo($_POST['tipo']);
+            // Persistir en doctrine y flush en BBDD
+            $entityManager->persist($newEmpresa);
+            $entityManager->flush();
+            // Regresamos a la vista /business
+            $this->getAll();
+        }
+
+        
+        
+
+    }
 }
